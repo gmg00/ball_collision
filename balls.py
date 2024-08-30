b115b21cfa4b0c466d04c6d7be2bc24402676e7e
gmg00 <gianmarcogori2000@gmail.com>
2024-08-30 16:07:10 +0200

    Updating balls.py

--- balls.py
+++ balls.py
@@ -52,13 +52,6 @@ while run:
         else: is_moving = True
         pygame.time.delay(100)
 
-    # if keys[pygame.K_r]:
-    #     win.fill((0, 0, 0))
-    #     x = 100
-    #     y = 250
-    #     theta = np.random.uniform(0, 2*np.pi)
-    #     v = [vel*np.cos(theta), vel*np.sin(theta)]
-
     if is_moving == True:
         if collision == True:
             for pair in itertools.combinations(balls, 2):
@@ -69,23 +62,11 @@ while run:
                 if np.linalg.norm(pos1-pos2) < ball_1.radius + ball_2.radius:
                     
                     ball_1, ball_2 = coll(ball_1, ball_2)
-                    # print(f'radius1 + radius2 = {ball_1.radius + ball_2.radius}')
-                    # print(f'pos1 - pos2 = {np.linalg.norm(np.array([ball_1.x, ball_1.y])-np.array([ball_2.x, ball_2.y]))}')
+                    
                     k_e = 0
                     for ball in balls:
                         k_e += ball.kinetic_energy()
-                    print(f'kinetic energy = {k_e}')
-                    #is_moving = False
-
-        # for ball in balls:
-        #     if ball.x > screen_width - ball.radius or ball.x < ball.radius:
-        #         overlap = np.abs()
-        #         ball.v[0] = -ball.v[0]
-        #     if ball.y > screen_width - ball.radius or ball.y < ball.radius:
-        #         ball.v[1] = -ball.v[1]
-            
-        #     ball.x += ball.v[0]
-        #     ball.y += ball.v[1]
+                    #print(f'kinetic energy = {k_e}')
 
         for ball in balls:
             if ball.x > screen_width - ball.radius:
@@ -104,7 +85,6 @@ while run:
                 ball.y -= overlap
                 ball.x -= overlap / ball.v[1] * ball.v[0]
                 ball.v[1] = -ball.v[1]
-
             elif ball.y < ball.radius:
                 overlap = np.abs(ball.radius - ball.y)
                 ball.y += overlap
@@ -114,14 +94,9 @@ while run:
             ball.x += ball.v[0]
             ball.y += ball.v[1]
 
-    #win.fill((0, 0, 0))
     win.blit(trail_surface, (0, 0))
     for ball in balls:
         pygame.draw.circle(win, ball.color, (ball.x, ball.y), ball.radius)
     pygame.display.update()
 
 pygame.quit()
-
-#la velocità relativa prima dell'urto è costante. Posso trovare la distanza relativa tra le due palle e sottraendo i due raggi trovare
-#la lunghezza di intersezione. Dopodiché con la velocità relativa e la lunghezza di intersezione tra le due palle calcolare il tempo al quale
-#c'è stato il contatto. Posso riportare indietro le palle sulla loro traiettoria di una distanza uguale alla loro velocità per il tempo.
